a65a61acb968cc7e7387a36310ab2b1ac37552fc
Safa <safa.dev.io@gmail.com>
2026-01-05 20:52:58 +0330

    Home Page Result

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,73 +1,86 @@
-import React from 'react';
-import RightSidebar from '../components/RightSidebar';
-import LeftSidebar from '../components/LeftSidebar';
-import Feed from '../components/Feed';
+import React from "react";
+import LeftSidebar from "../components/LeftSidebar";
+import Feed from "../components/Feed";
+import RightSidebar from "../components/RightSidebar";
 
 function HomePage() {
   return (
-    <div style={{ 
-      display: 'flex', 
-      maxWidth: '1400px', 
-      margin: '0 auto',
-      minHeight: '100vh',
-      position: 'relative',
-      backgroundColor: '#FFFFFF'
-    }}>
-      
-      {/* سایدبار چپ */}
-      <div style={{ 
-        width: '275px', 
-        height: '100vh',  // تمام ارتفاع viewport
-        overflowY: 'auto', // ← اسکرول جداگانه
-        position: 'sticky', 
-        top: 0,
-        alignSelf: 'flex-start'
-      }}>
-        <LeftSidebar />
-      </div>
-
-      {/* خط چپ */}
-      <div style={{
-        position: 'absolute',
-        left: '274px',
-        top: 0, bottom: 0,
-        width: '2px',
-        backgroundColor: '#EFF3F4'
-      }} />
-
-      {/* فید وسط - اسکرول اول */}
-      <div style={{ 
-        flex: 1, 
-        maxWidth: '600px',
+    <div 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
         minHeight: '100vh',
-        marginLeft: '2px',
-        overflowY: 'auto' // اسکرول مستقل
-      }}>
-        <Feed />
+        backgroundColor: '#FFFFFF',
+        overflowX: 'hidden'
+      }}
+    >
+      <div 
+        style={{
+          display: 'flex',
+          width: '100%',
+          maxWidth: 'clamp(600px, 90vw, 1225px)', // ← responsive
+          margin: '0 auto',
+          height: '100vh'
+        }}
+      >
+        {/* سایدبار چپ - responsive */}
+        <header 
+          style={{
+            width: 'clamp(200px, 20vw, 275px)', // 200px-275px
+            height: '100vh',
+            position: 'sticky',
+            top: 0,
+            borderRight: '1px solid #EFF3F4',
+            flexShrink: 0,
+            overflowY: 'auto',
+            overflowX: 'hidden',
+            scrollbarWidth: 'none',
+            backgroundColor: '#FFFFFF'
+          }}
+        >
+          <LeftSidebar />
+        </header>
+
+        {/* فید - responsive */}
+        <main 
+          style={{
+            width: 'clamp(400px, 55vw, 600px)', // 400px-600px
+            minWidth: 'clamp(400px, 55vw, 600px)',
+            borderLeft: '1px solid #EFF3F4',
+            borderRight: '1px solid #EFF3F4',
+            height: '100vh',
+            overflowY: 'auto',
+            overflowX: 'hidden',
+            backgroundColor: '#FFFFFF'
+          }}
+          className="feed-container"
+        >
+          <Feed />
+        </main>
+
+        {/* سایدبار راست - responsive */}
+        <aside 
+          style={{
+            width: 'clamp(250px, 25vw, 350px)', // 250px-350px
+            height: '100vh',
+            position: 'sticky',
+            top: 0,
+            paddingLeft: '20px',
+            paddingRight: '16px',
+            borderLeft: '1px solid #EFF3F4',
+            flexShrink: 0,
+            overflowY: 'auto',
+            overflowX: 'hidden',
+            scrollbarWidth: 'none',
+            backgroundColor: '#FFFFFF'
+          }}
+        >
+          <RightSidebar />
+          <style jsx>{`
+            aside::-webkit-scrollbar { display: none; }
+          `}</style>
+        </aside>
       </div>
-
-      {/* خط راست */}
-      <div style={{ 
-        position: 'absolute',
-        left: 'calc(275px + 2px + 600px + 2px)', 
-        top: 0, bottom: 0,
-        width: '2px',
-        backgroundColor: '#EFF3F4'
-      }} />
-
-      {/* سایدبار راست */}
-      <div style={{ 
-        width: '350px',
-        height: '100vh',  // تمام ارتفاع viewport
-        overflowY: 'auto', // ← اسکرول جداگانه
-        position: 'sticky', 
-        top: 0,
-        marginLeft: '1px',
-        alignSelf: 'flex-start'
-      }}>
-        <RightSidebar />
-      </div>
-
     </div>
   );
 }
